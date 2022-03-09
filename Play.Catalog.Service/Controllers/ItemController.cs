e1c24e41680bc43066275d8c287c0a497a4d7146
Hassan Mohamed <HassanMohamed_HM@hotmail.com>
2022-03-09 12:00:26 +0200

    Catalog Service

---
+++ Play.Catalog.Service/Controllers/ItemController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Play.Catalog.Service.Entities;
+using Play.Catalog.Service.Repositories;
+
+namespace Play.Catalog.Service.Controllers;
+[Route("items")]
+[ApiController]
+public class ItemController : ControllerBase
+{
+    #region Fileds :
+    private readonly IItemsRepository _itemsRepository;
+    #endregion
+
+    #region CTORS :
+    public ItemController(IItemsRepository itemsRepository)
+    {
+        this._itemsRepository = itemsRepository;
+    } 
+    #endregion
+
+    #region Endpoints :
+    //GET /items 
+    [HttpGet]
+    public async Task<IEnumerable<ItemDto>> GetAsync() => (await _itemsRepository.GetAllAsync()).Select(x => x.AsDto());
+    //GET /items/{id}
+    [HttpGet("{id:guid}")]
+    public async Task<ActionResult<ItemDto>> GetByIdAsync(Guid id)
+    {
+        var item = await _itemsRepository.GetAsync(id);
+        if (item is null) return NotFound();
+        return item.AsDto();
+    }
+    //POST /items
+    [HttpPost]
+    public async Task<ActionResult<ItemDto>> PostAsync(CreateItemDto itemDto)
+    {
+        Item item = new()
+        {
+            Id = Guid.NewGuid(),
+            Name = itemDto.Name,
+            Description = itemDto.Description,
+            Price = itemDto.Price,
+            CreatedDate = DateTimeOffset.UtcNow
+        };
+        await _itemsRepository.CreateAsync(item);
+        return CreatedAtAction(nameof(GetByIdAsync), new { item.Id }, item.AsDto());
+    }
+    //PUT /items/{id}
+    [HttpPut("{id:guid}")]
+    public async Task<IActionResult> PutAsync(Guid id, [FromBody] UpdateItemDto itemDto)
+    {
+        var item = await _itemsRepository.GetAsync(id);
+        if (item is null) return NotFound();
+        item.Name = itemDto.Name;
+        item.Description = itemDto.Description;
+        item.Price = itemDto.Price;
+        await _itemsRepository.UpdateAsync(item);
+        return NoContent();
+    }
+    //DELETE /items/{id}
+    [HttpDelete("{id:guid}")]
+    public async Task<IActionResult> DeleteAsync(Guid id)
+    {
+        var item = await _itemsRepository.GetAsync(id);
+        if (item is null) return NotFound();
+        await _itemsRepository.DeleteAsync(id);
+        return NoContent();
+    }
+    #endregion
+}
--- Play.Catalog.Service/Program.cs
+++ Play.Catalog.Service/Program.cs
@@ -1,8 +1,25 @@
+﻿using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Driver;
+using Play.Catalog.Service.Repositories;
+using Play.Catalog.Service.Settings;
+
 var builder = WebApplication.CreateBuilder(args);
-
+ServiceSettings serviceSettings= builder.Configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
+builder.Services.AddSingleton(serviceProvider =>
+{
+    MongoDbSettings mongoDbSettings = builder.Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
+    var mongoClient = new MongoClient(mongoDbSettings.ConnectionString);
+    return mongoClient.GetDatabase(serviceSettings.ServiceName);
+});
 // Add services to the container.
-
-builder.Services.AddControllers();
+BsonSerializer.RegisterSerializer(new GuidSerializer(MongoDB.Bson.BsonType.String));
+BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(MongoDB.Bson.BsonType.String));
+builder.Services.AddSingleton<IItemsRepository,ItemsRepository>();
+builder.Services.AddControllers(options =>
+{
+    options.SuppressAsyncSuffixInActionNames = false;
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
